1256b8d561a622bcc8f344de081645d3b70dd1ee
mu_tomoya <muton.tm.714@gmail.com>
2023-03-20 21:41:05 +0900

    youtube_id追加、ひろプリ追記ほか

--- src/graphql/model/precure.model.ts
+++ src/graphql/model/precure.model.ts
@@ -2,8 +2,8 @@ export interface Precure {
   id: number;
   name: string;
   cure_name: string;
-  series: string[];
-  item: string[];
+  series: string;
+  item: string;
   color: string;
   before_prologue: string | null;
   after_prologue: string | null;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,14 @@
-import Head from "next/head";
-import Image from "next/image";
 import styles from "@/styles/Home.module.css";
-
+import Link from "next/link";
 export default function Home() {
   return (
     <>
       <main className={styles.main}>
         <img src="/logo.png" alt="Grapn PreQL" className={styles.image} />
-        <a
-          className={styles.link}
-          href="https://graph-preql.vercel.app/api/graphql"
-        >
+        <Link className={styles.link} href="/api/graphql">
           GraphPreQLを試す
-        </a>
-        <img
-          src="/graph-preql.gif"
-          alt="Demo"
-          className={`${styles.image} ${styles.demo}`}
-        />
+        </Link>
+        <img src="/graph-preql.gif" alt="Demo" className={`${styles.image} ${styles.demo}`} />
       </main>
     </>
   );
